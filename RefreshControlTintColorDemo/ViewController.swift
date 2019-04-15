9d6c3fce38b5e79b26239dd34141d6bbca55fda6
uruly <rk.uru007@gmail.com>
2019-04-15 13:23:04 +0900

    :+1: Add CollectionView

--- RefreshControlTintColorDemo/ViewController.swift
+++ RefreshControlTintColorDemo/ViewController.swift
@@ -10,11 +10,37 @@ import UIKit
 
 class ViewController: UIViewController {
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+    @IBOutlet private weak var collectionView: UICollectionView! {
+        didSet {
+            collectionView.delegate = self
+            collectionView.dataSource = self
+            collectionView.register(UICollectionViewCell.self, forCellWithReuseIdentifier: "cell")
+        }
     }
 
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+}
+
+// MARK: - UICollectionViewDataSouce
+
+extension ViewController: UICollectionViewDataSource {
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 50
+    }
+
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "cell", for: indexPath)
+        cell.contentView.backgroundColor = UIColor.blue
+        return cell
+    }
+
+}
+// MARK: - UICollectionViewDelegate
+
+extension ViewController: UICollectionViewDelegate {
 
 }
 
